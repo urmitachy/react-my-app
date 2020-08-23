fd266e4111776731f4612a3504a2e81ce26ec501
urmitachy <ucmoon0703@gmail.com>
2020-08-23 12:16:30 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function App (){
+  const nayoks = ['Anwar','Jasim','Jafor','Alomgir','Salman','Bappi','Shuvo']
+const products = [
+  {name: 'Photoshop', price:'$90'},
+  {name :'illustrator', price:'$60.99'},
+  {name:'PDF', price:'40.99'}
+]
+const nayokNames =nayoks.map(nayok => nayok)
+console.log(nayokNames)
+ return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p>I am a react person</p>
+       <Counter></Counter>
+       <ul>
+         {
+           nayoks.map(nayok => <li>{nayok}</li>)
+         }
+         {
+           products.map(product => <li>{product.name}</li>)
+         }
+         </ul>
+         {
+           products.map(product =><Product product ={product}></Product>)
+         }
       </header>
     </div>
   );
 }
+
+function Counter(){
+  const [count, setCount] = useState(10);
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+  return(
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick ={handleIncrease}>Increase</button>
+    </div>
+  )
+}
+
+
+function Product(props){
+  const productStyle = {
+    border : '1px green solid',
+    borderRadius : '5px',
+    backgroundColor :'lightgray',
+    height :'200px',
+    width :'200px',
+    float :'left'
+
+  }
+  const {name, price}=props.product;
+  console.log(name, price)
+  return (
+    <div style={productStyle}>
+    <h3>{name} </h3>
+    <h5>{price}</h5>
+    <button>Buy now</button>
+    </div>
+  )
+}
+
+
+
+ function Person(props){
+   return(
+     <div>
+       <h3>My name : {props.name}</h3>
+       <p>My profession {props.job}: </p>
+     </div>
+   )
+ }
 
 export default App;
